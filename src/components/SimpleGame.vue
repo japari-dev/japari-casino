261b5b43b3b3a22784990865c20b2e2320b98bde
ysk_univ <ysk.univ.1007@gmail.com>
2020-04-30 00:50:09 +0900

    add SimpleGame.vue

---
+++ src/components/SimpleGame.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="simple-game">
+    <div>Total:</div>
+    <div>${{ total_except_bet.toLocaleString() }}.00</div>
+
+    <div>Your bet:</div>
+    <b-input-group prepend="$" append=".00">
+      <b-form-input v-model.number="bet" />
+    </b-input-group>
+
+    <b-button v-on:click="play" variant="primary">Play</b-button>
+
+    <div v-if="win != null">
+      <b-alert v-if="win" variant="success" show>You win!</b-alert>
+      <b-alert v-else variant="danger" show>You lose..</b-alert>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SimpleGame',
+  data: function () {
+    return {
+      total: 10,
+      bet: 0,
+      win: null
+    }
+  },
+  computed: {
+    total_except_bet: function () {
+      return this.total - this.bet
+    }
+  },
+  methods: {
+    play: function () {
+      this.total -= this.bet
+
+      var r = Math.floor(Math.random() * 2)
+
+      if (r === 1) {
+        this.win = true
+        this.total += this.bet * 2
+      } else {
+        this.win = false
+      }
+
+      this.bet = 0
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+</style>
