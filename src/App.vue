08ceb99d7b7f08508d504f1b8d2b04da52e25f91
ysk_univ <ysk.univ.1007@gmail.com>
2020-04-30 00:47:27 +0900

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
+    <b-navbar type="dark" variant="info">
+      <b-navbar-brand>Japari Casino</b-navbar-brand>
+    </b-navbar>
+
+    <SimpleGame />
   </div>
 </template>
 
 <script>
+import SimpleGame from './components/SimpleGame.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimpleGame
+  }
+}
 </script>
 
 <style>
