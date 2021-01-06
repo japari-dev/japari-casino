5cccd0e51a16de7baa8d2214b5e4ca48d754429d
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-06 23:31:04 +0900

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <b-navbar type="dark" variant="info">
+      <b-navbar-brand>Japari Casino</b-navbar-brand>
+
+      <b-navbar-nav> </b-navbar-nav>
+    </b-navbar>
+
     <router-view />
   </div>
 </template>
