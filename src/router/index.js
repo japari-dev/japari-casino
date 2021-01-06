2d79ffeca4c499e77c281f9cd94f5f2efee257b1
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-07 01:00:04 +0900

    add path to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import SimpleGame from '../views/SimpleGame.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/SimpleGame',
+    name: 'SimpleGame',
+    component: SimpleGame
+  }
 ]
 
 const router = new VueRouter({
