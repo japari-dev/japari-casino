8733d94a676fe0220dac8243479b9199bf7de848
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-07 01:05:34 +0900

    改行入れる

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <b-navbar type="dark" variant="info">
       <b-navbar-brand>Japari Casino</b-navbar-brand>
 
-      <b-navbar-nav> </b-navbar-nav>
+      <b-navbar-nav>
+      </b-navbar-nav>
     </b-navbar>
 
     <router-view />
