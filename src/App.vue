79a0899f0af015451b25958e5930e49b2e4207c4
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-08 00:18:13 +0900

    add item to navbar

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 
       <b-navbar-nav>
         <b-nav-item href="#/SimpleGame">Simple Game</b-nav-item>
+        <b-nav-item href="#/HighLow">High Low</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
 
