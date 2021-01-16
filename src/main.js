6d3fb43e7af1891f5011a2a86880701ee9478acb
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-16 14:49:32 +0900

    register component

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import Button from './components/Button'
 
 Vue.config.productionTip = false
+
+Vue.component('Button', Button)
 
 new Vue({
   router,
