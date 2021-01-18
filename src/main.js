f71a1f57b5b28c81d29e7ac0119233176d272746
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-18 23:35:25 +0900

    register component

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue'
 import router from './router'
 import AmountInput from './components/AmountInput'
 import Button from './components/Button'
+import WinLoseAlert from './components/WinLoseAlert'
 
 Vue.config.productionTip = false
 
 Vue.component('AmountInput', AmountInput)
 Vue.component('Button', Button)
+Vue.component('WinLoseAlert', WinLoseAlert)
 
 new Vue({
   router,
