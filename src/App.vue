f11a2f7d9e87a92ae8db4181fedbae5b26999355
ysk_univ <ysk.univ.1007@gmail.com>
2021-01-27 01:16:10 +0900

    [src/App.vue] add Home

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,19 @@
         ><img src="./assets/logo.png" width="220pm"
       /></b-navbar-brand>
     </b-navbar>
+
+    <Home />
   </div>
 </template>
 
 <script>
+import Home from './views/Home'
+
+export default {
+  components: {
+    Home: Home
+  }
+}
 </script>
 
 <style>
